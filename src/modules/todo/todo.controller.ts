1cb847b222b2ee90ce1d573d43936139e0346f31
sayed725 <ssayed72533@gmail.com>
2025-12-03 21:00:28 +0600

    done adding all crud op of todos

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,15 +1,10 @@
 import { Request, Response } from "express";
 import { todoServices } from "./todo.service";
 
-
-
-
-
 const createTodo = async (req: Request, res: Response) => {
-
   try {
     const result = await todoServices.createTodo(req.body);
-    
+
     res.status(201).json({
       success: true,
       message: "Todos created successfully",
@@ -23,7 +18,6 @@ const createTodo = async (req: Request, res: Response) => {
     });
   }
 };
-
 
 const getTodos = async (req: Request, res: Response) => {
   try {
@@ -42,9 +36,75 @@ const getTodos = async (req: Request, res: Response) => {
   }
 };
 
+const getSingleTodo = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
+  try {
+    const result = await todoServices.getSingleTodo(id as string);
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Todos not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Todos fetched successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+};
+
+const updateTodo = async (req: Request, res: Response) => {
+  try {
+    const result = await todoServices.updateTodo(req.body, req.params.id!);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Failed to update todo" });
+  }
+};
+
+const deleteTodo = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await todoServices.deleteTodo(id as string);
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Todos not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Todos deleted successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+};
 
 export const todoControllers = {
   createTodo,
   getTodos,
+  getSingleTodo,
+  updateTodo,
+  deleteTodo,
 };
--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -1,7 +1,5 @@
 import { pool } from "../../config/db";
 
-
-
 // Record<string, unknown> = {key: value}
 const createTodo = async (payload: Record<string, unknown>) => {
   const { user_id, title } = payload;
@@ -13,17 +11,36 @@ const createTodo = async (payload: Record<string, unknown>) => {
   return result;
 };
 
-
 const getTodos = async () => {
   const result = await pool.query(`SELECT * FROM todos`);
   return result;
 };
 
+const getSingleTodo = async (id: string) => {
+  const result = await pool.query("SELECT * FROM todos WHERE id = $1", [id]);
+  return result;
+};
 
+const updateTodo = async (payload: Record<string, unknown>, id: string) => {
+  const { title } = payload;
+  const result = await pool.query(
+    "UPDATE todos SET title=$1 WHERE id=$2 RETURNING *",
+    [title, id]
+  );
+  return result;
+};
+
+const deleteTodo = async (id: string) => {
+  const result = await pool.query("DELETE FROM todos WHERE id=$1 RETURNING *", [
+    id,
+  ]);
+  return result;
+};
 
 export const todoServices = {
   createTodo,
-  getTodos
-
+  getTodos,
+  getSingleTodo,
+  updateTodo,
+  deleteTodo,
 };
-
